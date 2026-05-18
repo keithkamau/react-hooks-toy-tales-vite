e05ffc79852a408eb0e4e6c4c4176c0d5b4cbb1d
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-05-18 20:11:46 +0300

    feat: fetch and display all toys on page load

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import ToyForm from "./ToyForm";
+import ToyContainer from "./ToyContainer";
 
-function Header() {
+function App() {
+  const [showForm, setShowForm] = useState(false);
+  // State to hold the array of toys fetched from the backend
+  const [toys, setToys] = useState([]);
+
+  // Fetch all toys from the backend when the component first mounts
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, []); // Empty dependency array = runs once on mount
+
+  function handleClick() {
+    setShowForm((showForm) => !showForm);
+  }
+
   return (
-    <div id="toy-header">
-      <img
-        src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
-        alt="toy header"
-      />
-    </div>
+    <>
+      <Header />
+      {showForm ? <ToyForm /> : null}
+      <div className='buttonContainer'>
+        <button onClick={handleClick}>Add a Toy</button>
+      </div>
+      {/* Pass toys array down to ToyContainer */}
+      <ToyContainer toys={toys} />
+    </>
   );
 }
 
-export default Header;
+export default App;
